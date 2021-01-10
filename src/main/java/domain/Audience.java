444b01eb3b6073dcde38f9b094f261c069cc4da8
ssibongee <ssibongee@gmail.com>
2021-01-10 17:04:16 +0900

    [#1] TicketSeller가 Audience의 인터페이스에만 의존하도록 리팩토링하여 결합도를 낮춤

--- src/main/java/domain/Audience.java
+++ src/main/java/domain/Audience.java
@@ -2,12 +2,22 @@ package domain;
 
 import lombok.Getter;
 
-@Getter
 public class Audience {
 
     private Bag bag;
 
     public Audience(Bag bag) {
         this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        if(bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
